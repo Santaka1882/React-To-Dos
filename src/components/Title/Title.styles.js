f3ae35b8521c792a2290e97513eb3a31d70c8995
Cristian Buitrago <Crisis1882@gmail.com>
2021-10-05 13:13:01 -0500

    Ultra necesary feature

--- src/components/Title/Title.styles.js
+++ src/components/Title/Title.styles.js
@@ -8,7 +8,16 @@ export const Wrapper = styled.div`
 `
 
 export const Content = styled.div`
+  display: flex;
+  justify-content: ${props => props.name === 'Luna' ? 'space-between' : 'center'};
+  align-items: center;
+  width: 90%;
+
   h1 {
     color: var(--blue);
+  }
+  
+  img {
+    width: 40px;
   }
 `
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 // Styles
 import { Wrapper, Content } from "./Title.styles";
+import turtle from '../../images/turtle.png'
 
 const Title = ({ name }) => (
   <Wrapper>
-    <Content>
-      <h1>{name}'s To-dos</h1>
+    <Content name={name}>
+      {name === 'Luna' ? 
+        <>
+        <img src={turtle} alt="tortuga EE"/>
+        <h1>{name}'s To-dos</h1>
+        <img src={turtle} alt="tortuga EE"/>
+        </>
+       : <h1>{name}'s To-dos</h1>
+      }
     </Content>
   </Wrapper>
 )
